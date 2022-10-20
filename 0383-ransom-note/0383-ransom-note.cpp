59194f9099dada41f06ea16563645d8849da410b
Bellahuang3715 <98608943+Bellahuang3715@users.noreply.github.com>
2022-10-20 00:08:31 -0400

    Time: 38 ms (29.99%), Space: 8.9 MB (40.26%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> charCount;
+        for (auto c: magazine) {
+            charCount[c]++;
+        }
+        for (auto c: ransomNote) {
+            charCount[c]--;
+        }
+        for (auto &ele: charCount) {
+            if (ele.second < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
