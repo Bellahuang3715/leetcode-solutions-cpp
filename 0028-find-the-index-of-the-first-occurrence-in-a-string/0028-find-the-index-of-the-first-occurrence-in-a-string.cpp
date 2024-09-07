935aece6db7e5a6587b29a54fd1567a4a94fa437
Bellahuang3715 <98608943+Bellahuang3715@users.noreply.github.com>
2024-09-07 19:38:17 -0400

    Time: 0 ms (100.00%), Space: 8.3 MB (20.25%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        // sliding window algorithm
+        if (needle.empty()) return 0;
+        if (needle.length() > haystack.length()) return -1;
+        
+        for (int i = 0; i <= (haystack.length() - needle.length()); i++)
+        {
+            if (haystack.substr(i, needle.length()) == needle)
+                return i;
+        }
+        return -1;
+    }
+};
