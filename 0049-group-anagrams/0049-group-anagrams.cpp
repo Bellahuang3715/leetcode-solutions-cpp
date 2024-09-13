cbdedbc9f812a39bc145a4825c5dedda47abaf5d
Bellahuang3715 <98608943+Bellahuang3715@users.noreply.github.com>
2024-09-13 16:47:28 -0400

    Time: 25 ms (72.80%), Space: 24.1 MB (76.61%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         // hash map
         unordered_map<string, vector<string>> strMap;
-        for (auto str : strs)
+        for (auto& str : strs)
         {
             string word = str;
             sort(word.begin(), word.end());
@@ -11,7 +11,7 @@ public:
         }
         
         vector<vector<string>> result;
-        for (auto group : strMap)
+        for (auto& group : strMap)
         {
             result.push_back(group.second);
         }
