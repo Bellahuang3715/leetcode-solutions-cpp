330a8e7a54263e410e8154ce0cd8febf5b763e6d
Bellahuang3715 <98608943+Bellahuang3715@users.noreply.github.com>
2024-10-11 19:59:44 -0400

    Time: 34 ms (29.11%), Space: 23.7 MB (24.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    // depth first search
     void dfs(vector<vector<char>>& grid, int row, int col)
     {
         if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() || grid[row][col] == '0') { return; }
@@ -9,6 +9,40 @@ public:
         dfs(grid, row-1, col);
         dfs(grid, row, col+1);
         dfs(grid, row, col-1);
+    }
+    
+    // breath first search
+    void bfs(vector<vector<char>>& grid, int row, int col)
+    {
+        // positions to visit
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        
+        // arrays to help traverse the 4 directions (up, down, left, right)
+        vector<int> directionX = {1, -1, 0, 0};  // down, up, right, left
+        vector<int> directionY = {0, 0, 1, -1};  // down, up, right, left
+        
+        while (!q.empty())
+        {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            
+            for (int i = 0; i < 4; i++)
+            {
+                int newX = x + directionX[i];
+                int newY = y + directionY[i];
+                
+                if (newX >= 0 && newX < grid.size() && newY >= 0 && newY < grid[0].size())
+                {
+                    if (grid[newX][newY] == '1')
+                    {
+                        grid[newX][newY] = '0';
+                        q.push({newX, newY});
+                    }
+                }
+            }
+        }
     }
     
     int numIslands(vector<vector<char>>& grid) {
@@ -23,7 +57,8 @@ public:
             {
                 if (grid[i][j] == '1')
                 {
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
+                    // dfs(grid, i, j);
                     islands++;
                 }
             }
