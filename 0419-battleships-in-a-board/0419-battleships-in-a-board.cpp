600b5d0c8571566e231c219e32162eadb0d66482
Bellahuang3715 <98608943+Bellahuang3715@users.noreply.github.com>
2024-10-12 12:41:37 -0400

    Time: 10 ms (22.69%), Space: 14.3 MB (5.05%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    void bfs(vector<vector<char>>& board, int row, int col)
+    {
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        while (!q.empty())
+        {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            
+            vector<int> dirX = {1, -1, 0, 0};
+            vector<int> dirY = {0, 0, 1, -1};
+            
+            for (int i = 0; i < 4; i++)
+            {
+                int newX = x + dirX[i];
+                int newY = y + dirY[i];
+                
+                if (newX >= 0 && newX < board.size() && newY >= 0 && newY < board[0].size() && board[newX][newY] != '.')
+                {
+                    if (board[newX][newY] == 'X')
+                    {
+                        board[newX][newY] = '.';
+                        q.push({newX, newY});
+                    }
+                }
+            }
+        }
+    }
+    
     void dfs(vector<vector<char>>& board, int row, int col)
     {
         if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] == '.')
@@ -24,7 +54,8 @@ public:
                 if (board[i][j] == 'X')
                 {
                     battleships++;
-                    dfs(board, i, j);
+                    // dfs(board, i, j);
+                    bfs(board, i, j);
                 }
             }
         }
